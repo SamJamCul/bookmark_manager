8924ce2450a32984b6129a33a620cee2160663d4
Aga <a.dufrat@gmail.com>
2018-11-21 14:57:00 +0000

    Completes Adding new bookmark feature

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,9 +1,11 @@
 require 'pg'
 
 class Bookmark
-  attr_reader :url
-  def initialize(url)
+  attr_reader :url, :id, :title
+  def initialize(id:, title:, url:)
+    @id = id
     @url = url
+    @title = title
   end
 
   def self.all
@@ -13,7 +15,9 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
     result = connection.exec("SELECT * FROM bookmarks;")
-    result.map { |bookmark| bookmark }
+    result.map do |bookmark|
+      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    end
   end
 
   def self.create(url:, title:)
@@ -23,7 +27,8 @@ class Bookmark
       connection = PG.connect(dbname: 'bookmark_manager')
     end
 
-    connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, url, title")
+    result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, url, title")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
 end
